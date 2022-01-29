b5a832df1431423b5a05d53f13ba25991ddcf44b
ValmirMujku <valmir.mujku@gmail.com>
2022-01-29 13:15:20 +0100

    aboutdetailss

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import PreEntry from './Components/PreEntry/PreEntry'; 
+ 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <PreEntry/> */}
+    
     <App />
   </React.StrictMode>,
   document.getElementById('root')
